c4539f7a75dadfbec51e5121dcd68a6a7f9b15af
Matheus Henrique <matheushnt06@gmail.com>
2025-08-07 10:12:39 -0300

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import { React } from 'react';
 
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
 const App = () => {
+  const dados = luana;
+
+  const total = dados.compras
+    .map(compra => Number(compra.preco.replace('R$ ', '')))
+    .reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+
   return (
-    <div>
-      App React <button>Clique</button>
-    </div>
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{' '}
+        {dados.ativa ? (
+          <span style={{ color: 'green' }}>Ativa</span>
+        ) : (
+          <span style={{ color: 'red' }}>Inativa</span>
+        )}
+      </p>
+      <p>Total gasto: R$ {total}</p>
+      {total > 10000 && <p>Você está gastando muito.</p>}
+    </>
   );
 };
 
