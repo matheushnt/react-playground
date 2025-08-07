62b32daf74a30b22bbbb06ce59d4b622278bb0fc
Matheus Henrique <matheushnt06@gmail.com>
2025-08-07 18:22:21 -0300

    JSX Arrays 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,45 @@
 import { React } from 'react';
 
-const App = () => {
-  const filmes = ['Guardiões da Galáxia', 'Homem Aranha', 'Thor'];
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
-  const livros = [
-    { nome: 'A Game of Thrones', ano: 1996 },
-    { nome: 'A Clash of Kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ];
+const App = () => {
+  const produtosFiltrados = produtos.filter(({ preco }) => Number(preco.replace('R$ ', '')) > 1500);
 
   return (
-    <>
-      <ul>
-        {filmes.map((filme, index) => (
-          <li key={index}>{filme}</li>
-        ))}
-      </ul>
-      <ul>
-        {livros
-          .filter(({ ano }) => ano >= 1998)
-          .map(({ nome, ano }) => (
-            <li key={nome}>
-              {nome}, {ano}
-            </li>
-          ))}
-      </ul>
-    </>
+    <section>
+      {produtosFiltrados.map(produto => (
+        <div key={produto.id}>
+          <h1>{produto.nome}</h1>
+          <p>Preço: {produto.preco}</p>
+          <ul>
+            {produto.cores.map(cor => (
+              <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
   );
 };
 
