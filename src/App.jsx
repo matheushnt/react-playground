80501f23e7a05f1a7795507235da21d4d3f19eeb
Matheus Henrique <matheushnt06@gmail.com>
2025-08-07 15:08:04 -0300

    JSX Arrays 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,30 @@
 import { React } from 'react';
 
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};
 const App = () => {
-  const dados = luana;
+  const filmes = ['Guardiões da Galáxia', 'Homem Aranha', 'Thor'];
 
-  const total = dados.compras
-    .map(compra => Number(compra.preco.replace('R$ ', '')))
-    .reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+  const livros = [
+    { nome: 'A Game of Thrones', ano: 1996 },
+    { nome: 'A Clash of Kings', ano: 1998 },
+    { nome: 'A Storm of Swords', ano: 2000 },
+  ];
 
   return (
     <>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{' '}
-        {dados.ativa ? (
-          <span style={{ color: 'green' }}>Ativa</span>
-        ) : (
-          <span style={{ color: 'red' }}>Inativa</span>
-        )}
-      </p>
-      <p>Total gasto: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito.</p>}
+      <ul>
+        {filmes.map((filme, index) => (
+          <li key={index}>{filme}</li>
+        ))}
+      </ul>
+      <ul>
+        {livros
+          .filter(({ ano }) => ano >= 1998)
+          .map(({ nome, ano }) => (
+            <li key={nome}>
+              {nome}, {ano}
+            </li>
+          ))}
+      </ul>
     </>
   );
 };
