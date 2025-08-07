dbb38388ba893630d1ac3b375e9b2ef6dafb1213
Matheus Henrique <matheushnt06@gmail.com>
2025-08-07 19:08:45 -0300

    Eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
 import { React } from 'react';
 
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
-
 const App = () => {
-  const produtosFiltrados = produtos.filter(({ preco }) => Number(preco.replace('R$ ', '')) > 1500);
+  window.addEventListener('scroll', ({ target }) => console.log(target));
 
   return (
-    <section>
-      {produtosFiltrados.map(produto => (
-        <div key={produto.id}>
-          <h1>{produto.nome}</h1>
-          <p>Preço: {produto.preco}</p>
-          <ul>
-            {produto.cores.map(cor => (
-              <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
-                {cor}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
+    <div style={{ height: '100vh' }}>
+      <button onClick={({ target }) => alert(target.textContent)}>Clique</button>
+    </div>
   );
 };
 
