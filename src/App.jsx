0e33bd03f01283996428231dd95fd0fc664503c5
Matheus Henrique <matheushnt.dev@gmail.com>
2025-08-14 22:50:02 -0300

    Custom Hook useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import useLocalStorage from './useLocalStorage';
+import React, { useEffect } from 'react';
+import useFetch from './useFetch';
 
 const App = () => {
-  const [produto, setProduto] = useLocalStorage('produto', '');
+  const { request, data, loading, error } = useFetch();
 
-  function handleClick({ target }) {
-    setProduto(target.innerText);
+  useEffect(() => {
+    async function fetchData() {
+      const { response, json } = await request('https://ranekapi.origamid.dev/json/api/produto/');
+    }
+
+    fetchData();
+  }, [request]);
+
+  if (error) {
+    return <p>Ocorreu um erro: {error}</p>;
   }
 
-  return (
-    <div>
-      <p>Produto preferido: {produto}</p>
-      <button onClick={handleClick}>notebook</button>
-      <button onClick={handleClick}>smartphone</button>
-    </div>
-  );
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  if (data) {
+    return (
+      <div>
+        {data.map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default App;
---
+++ src/useFetch.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useCallback } from 'react';
+
+const useFetch = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const request = useCallback(async (url, options) => {
+    let response;
+    let json;
+
+    try {
+      setError(null);
+      setLoading(true);
+      const response = await fetch(url, options);
+      const json = await response.json();
+      setLoading(false);
+    } catch (erro) {
+      json = null;
+      setError('Erro');
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return { data, error, loading, request };
+};
+
+export default useFetch;
